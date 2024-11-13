1abc64d65486c3ce31caa37816736660a1b46830
Tech2cool <126472927+Tech2cool@users.noreply.github.com>
2024-11-13 22:32:44 +0530

    update search

--- src/server.js
+++ src/server.js
@@ -8,9 +8,39 @@ app.use(cors());
 const ytmusic = new YTMusic();
 await ytmusic.initialize();
 
+app.get("/search/:type?", async (req, res) => {
+  const { type } = req.params;
+  const { query = " " } = req.query;
+  try {
+    if (type === "SONG") {
+      const resp = await ytmusic.searchSongs(query);
+      return res.send({ data: resp });
+    } else if (type === "ALBUM") {
+      const resp = await ytmusic.searchAlbums(query);
+      return res.send({ data: resp });
+    } else if (type === "ARTIST") {
+      const resp = await ytmusic.searchArtists(query);
+      return res.send({ data: resp });
+    } else if (type === "VIDEO") {
+      const resp = await ytmusic.searchVideos(query);
+      return res.send({ data: resp });
+    }
+
+    const resp = await ytmusic.search(query);
+    // const resp2 = await ytmusic.searchVideos(query);
+    // const resp2 = await ytmusic.searchPlaylists(query);
+    // const resp2 = await ytmusic.searchAlbums(query);
+    // const resp2 = await ytmusic.searchArtists(query);
+    // const resp2 = await ytmusic.getSearchSuggestions(query);
+    return res.send({ data: resp });
+  } catch (error) {
+    return res.send(error);
+  }
+});
+
 app.get("/", async (req, res) => {
   try {
-    const resp = await ytmusic.search("Never gonna give you up");
+    const resp = await ytmusic.search(req.query.query);
     res.send(resp);
   } catch (error) {
     res.send(error);
